721f1c1701e10393d1471283426e165cef4d936b
swx12138 <zhaolc_bzh@163.com>
2022-07-25 16:36:36 +0800

    message

--- GenshinData/settings.py
+++ GenshinData/settings.py
@@ -114,6 +114,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "GenshinData/Templates/css",
+    BASE_DIR / "GenshinData/Templates/js",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
